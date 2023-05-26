5388bb6b806215881008936a29b875170e6eca62
unknown <icyveins@hotmail.com>
2023-05-26 18:23:39 +0800

    add basic matrix class which inherits vector
    goal is to make things more accessible, but may have to give up resizing

---
+++ include/ipp_ext_matrix.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "ipp_ext_vec.h"
+
+namespace ippe
+{
+    template <typename T>
+    class matrix : public vector<T>
+    {
+        public:
+            // Constructors
+            matrix()
+                : vector<T>()
+            {
+                // do nothing
+            }
+
+            // Standard constructor
+            explicit matrix(size_t rows, size_t columns)
+                : vector<T>(rows * columns), m_rows(rows), m_columns(columns)
+            {
+                
+            }
+
+            // Construct with certain value
+            matrix(size_t rows, size_t columns, const T& value)
+                : vector<T>(rows * columns, value), m_rows(rows), m_columns(columns)
+            {
+                
+            }
+
+            // Copy constructor
+            matrix(const matrix& other)
+                : vector<T>(other), m_rows(other.m_rows), m_columns(other.m_columns)
+            {
+
+            }
+
+            // Assignment operator
+            matrix& operator=(const matrix& other)
+            {
+                m_rows = other.m_rows;
+                m_columns = other.m_columns;
+                vector<T>::operator=(other);
+                return *this;
+            }
+
+            // Destructor
+            ~matrix()
+            {
+                
+            }
+
+            // Extra accessors
+            size_t rows()
+            {
+                return m_rows;
+            }
+
+            size_t columns()
+            {
+                return m_columns;
+            }
+
+            // Access a row
+            T& row(size_t rowIdx)
+            {
+                if (rowIdx >= m_rows)
+                {
+                    throw std::out_of_range("Row index out of range");
+                }
+                else
+                    return m_data[m_columns * rowIdx];
+            }
+
+            // Access a row and column
+            T& index(size_t rowIdx, size_t columnIdx)
+            {
+                if (rowIdx >= m_rows || columnIdx >= m_columns)
+                    throw std::out_of_range("Index out of range");
+                else
+                    return m_data[rowIdx * m_columns + columnIdx];
+            }
+
+            // Remove size adjustment methods (for now?)
+            void push_back(T value) = delete;
+            void resize(size_t new_count) = delete;
+            void resize(size_t new_count, const T& value) = delete;
+
+        private:
+            size_t m_rows;
+            size_t m_columns;
+    }
+}
