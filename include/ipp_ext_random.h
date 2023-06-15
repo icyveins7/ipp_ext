c6ce02196f9825c39093a7d6c97abf83a0767261
seoxubuntu <icyveins@hotmail.com>
2023-06-15 19:55:51 +0800

    add RandGauss and tests (failing)

--- include/ipp_ext_random.h
+++ include/ipp_ext_random.h
@@ -7,13 +7,20 @@
 
 namespace ippe
 {
+    /*
+    =====================
+    UNIFORM DISTRIBUTION
+    =====================
+    */
     template <typename T>
     class RandUniform
     {
     public:
+        /// @brief Instantiates a RandUniform object with default values.
+        /// Low: 0.0, High: 1.0. Seed: 0.
         RandUniform()
         {
-
+            prepare();
         }
 
         /// @brief Instantiates a RandUniform object with a particular seed.
@@ -30,7 +37,7 @@ namespace ippe
         /// @param low Lower bound of values.
         /// @param high Upper bound of values (inclusive).
         RandUniform(T low, T high)
-            : RandUniform(low, high, 0)
+            : m_low{low}, m_high{high}, m_seed{0}
         {
             prepare();
         }
@@ -50,7 +57,7 @@ namespace ippe
 
         T getLow(){ return m_low; }
         T getHigh(){ return m_high; }
-        T getSeed(){ return m_seed; }
+        unsigned int getSeed(){ return m_seed; }
 
     private:
         T m_low = 0;
@@ -188,5 +195,190 @@ namespace ippe
         IppStatus sts = ippsRandUniform_64f(
             data, length, (IppsRandUniState_64f*)rndObj.data()); 
         IPP_NO_ERROR(sts, "ippsRandUniform_64f");
+    }
+
+
+
+    /*
+    =====================
+    GAUSSIAN DISTRIBUTION
+    =====================
+    */
+    template <typename T>
+    class RandGauss
+    {
+    public:
+        /// @brief Instantiates a RandGauss object with default values.
+        /// Mean: 0.0, Stddev: 1.0. Seed: 0.
+        RandGauss()
+        {
+            prepare();
+        }
+
+        /// @brief Instantiates a RandGauss object with a particular seed.
+        /// @param mean Mean of Gaussian distribution.
+        /// @param stddev Standard deviation of Gaussian distribution.
+        /// @param seed Seed for random number generator.
+        RandGauss(T mean, T stddev, unsigned int seed)
+            : m_mean{mean}, m_stddev{stddev}, m_seed{seed}
+        {
+            prepare();
+        }
+
+        /// @brief Instantiates a RandGauss object with a seed of 0.
+        /// @param mean Mean of Gaussian distribution.
+        /// @param stddev Standard deviation of Gaussian distribution.
+        RandGauss(T mean, T stddev)
+            : m_mean{mean}, m_stddev{stddev}, m_seed{0}
+        {
+            prepare();
+        }
+
+        /// @brief Writes gaussianly distributed random values at an array pointer.
+        /// @param data The output array pointer.
+        /// @param length Number of values to write.
+        void generate(T* data, int length);
+
+        // Getters
+
+        T getMean(){ return m_mean; }
+        T getStddev(){ return m_stddev; }
+        unsigned int getSeed(){ return m_seed; }
+
+    private:
+        T m_mean = 0;
+        T m_stddev = 1;
+        unsigned int m_seed = 0;
+        ippe::vector<Ipp8u> rndObj;
+
+        void prepare();
+    };
+
+    // Template specializations for prepare
+
+    template <typename T>
+    inline void RandGauss<T>::prepare()
+    {
+        throw std::runtime_error("RandGauss not implemented for generic types.");
+    }
+
+    // Ipp8u
+
+    template <>
+    inline void RandGauss<Ipp8u>::prepare()
+    {
+        IppStatus sts;
+        // Get spec size
+        int sizeRndObj;
+        sts = ippsRandGaussGetSize_8u(&sizeRndObj);
+        IPP_NO_ERROR(sts, "ippsRandGaussGetSize_8u");
+
+        // Allocate memory
+        rndObj.resize(sizeRndObj);
+        // Initialize it
+        sts = ippsRandGaussInit_8u((IppsRandGaussState_8u*)rndObj.data(), m_mean, m_stddev, m_seed);
+        IPP_NO_ERROR(sts, "ippsRandGaussInit_8u");
+    }
+
+    // Ipp16s
+
+    template <>
+    inline void RandGauss<Ipp16s>::prepare()
+    {
+        IppStatus sts;
+        // Get spec size
+        int sizeRndObj;
+        sts = ippsRandGaussGetSize_16s(&sizeRndObj);
+        IPP_NO_ERROR(sts, "ippsRandGaussGetSize_16s");
+
+        // Allocate memory
+        rndObj.resize(sizeRndObj);
+        // Initialize it
+        sts = ippsRandGaussInit_16s((IppsRandGaussState_16s*)rndObj.data(), m_mean, m_stddev, m_seed);
+        IPP_NO_ERROR(sts, "ippsRandGaussInit_16s");
+    }
+
+    // Ipp32f
+
+    template <>
+    inline void RandGauss<Ipp32f>::prepare()
+    {
+        IppStatus sts;
+        // Get spec size
+        int sizeRndObj;
+        sts = ippsRandGaussGetSize_32f(&sizeRndObj);
+        IPP_NO_ERROR(sts, "ippsRandGaussGetSize_32f");
+
+        // Allocate memory
+        rndObj.resize(sizeRndObj);
+        // Initialize it
+        sts = ippsRandGaussInit_32f((IppsRandGaussState_32f*)rndObj.data(), m_mean, m_stddev, m_seed);
+        IPP_NO_ERROR(sts, "ippsRandGaussInit_32f");
+    }
+
+    // Ipp64f
+
+    template <>
+    inline void RandGauss<Ipp64f>::prepare()
+    {
+        IppStatus sts;
+        // Get spec size
+        int sizeRndObj;
+        sts = ippsRandGaussGetSize_64f(&sizeRndObj);
+        IPP_NO_ERROR(sts, "ippsRandGaussGetSize_64f");
+
+        // Allocate memory
+        rndObj.resize(sizeRndObj);
+        // Initialize it
+        sts = ippsRandGaussInit_64f((IppsRandGaussState_64f*)rndObj.data(), m_mean, m_stddev, m_seed);
+        IPP_NO_ERROR(sts, "ippsRandGaussInit_64f");
+    }
+
+    // Template specializations for generate
+
+    template <typename T>
+    inline void RandGauss<T>::generate(T* data, int length)
+    {
+        throw std::runtime_error("RandGauss not implemented for generic types.");
+    }
+
+    // Ipp8u
+
+    template <>
+    inline void RandGauss<Ipp8u>::generate(Ipp8u* data, int length)
+    {
+        IppStatus sts = ippsRandGauss_8u(
+            data, length, (IppsRandGaussState_8u*)rndObj.data()); 
+        IPP_NO_ERROR(sts, "ippsRandGauss_8u");
+    }
+
+    // Ipp16s
+
+    template <>
+    inline void RandGauss<Ipp16s>::generate(Ipp16s* data, int length)
+    {
+        IppStatus sts = ippsRandGauss_16s(
+            data, length, (IppsRandGaussState_16s*)rndObj.data());
+        IPP_NO_ERROR(sts, "ippsRandGauss_16s");
+    }
+
+    // Ipp32f
+
+    template <>
+    inline void RandGauss<Ipp32f>::generate(Ipp32f* data, int length)
+    {
+        IppStatus sts = ippsRandGauss_32f(
+            data, length, (IppsRandGaussState_32f*)rndObj.data());
+        IPP_NO_ERROR(sts, "ippsRandGauss_32f");
+    }
+
+    // Ipp64f
+
+    template <>
+    inline void RandGauss<Ipp64f>::generate(Ipp64f* data, int length)
+    {
+        IppStatus sts = ippsRandGauss_64f(
+            data, length, (IppsRandGaussState_64f*)rndObj.data());
+        IPP_NO_ERROR(sts, "ippsRandGauss_64f");
     }
 }
--- tests/test_random.cpp
+++ tests/test_random.cpp
@@ -5,7 +5,7 @@
 
 
 /*
-Template instantiation test case
+Template test case for RandUniform
 */
 template <typename T>
 void randUniform_test()
@@ -85,5 +85,93 @@ TEST_CASE("ippe RandUniform", "[random], [uniform]")
     SECTION("Ipp64f")
     {
         randUniform_test<Ipp64f>();
+    }
+}
+
+/*
+Template test case for RandGauss
+*/
+template <typename T>
+void randGauss_test()
+{
+    // Create generator with seed
+    ippe::RandGauss<T> gen(10, 1, 100);
+
+    // Check internals
+    REQUIRE(gen.getMean() == 10);
+    REQUIRE(gen.getStddev() == 1);
+    REQUIRE(gen.getSeed() == 100);
+
+    // Create without seed
+    ippe::RandGauss<T> gen2(0, 1);
+
+    // Check internals
+    REQUIRE(gen2.getMean() == 0);
+    REQUIRE(gen2.getStddev() == 1);
+    REQUIRE(gen2.getSeed() == 0);
+
+    // Create another via automatic copy constructor
+    ippe::RandGauss<T> gen3 = gen2;
+
+    // Generate some data
+    ippe::vector<T> data(1000);
+    ippe::vector<T> data2(1000);
+    ippe::vector<T> data3(1000);
+
+    gen.generate(data.data(), 1000);
+    gen2.generate(data2.data(), 1000);
+    gen3.generate(data3.data(), 1000);
+
+    // Same seed should be equal
+    for (int i = 0; i < 1000; i++)
+    {
+        REQUIRE(data2[i] == data3[i]);
+    }
+
+    // There should be at least 1 different value when seeds are different
+    bool isDifferent = false;
+    for (int i = 0; i < 1000; i++)
+    {
+        if (data[i] != data2[i])
+        {
+            isDifferent = true;
+            break;
+        }
+    }
+    REQUIRE(isDifferent);
+
+    // Can't really definitively test for stddeviation/mean values, nor range of values
+    T estMean = 0, estMean2 = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        estMean += data[i];
+        estMean2 += data2[i];
+    }
+    estMean = estMean / (T)1000;
+    estMean2 = estMean2 / (T)1000;
+
+    REQUIRE(estMean > estMean2); // First one should be sufficiently bigger
+}
+
+TEST_CASE("ippe RandGauss", "[random], [gauss]")
+{
+    SECTION("Ipp8u")
+    {
+        randGauss_test<Ipp8u>();
+    }
+
+    SECTION("Ipp16s")
+    {
+        randGauss_test<Ipp16s>();
+    }
+
+    SECTION("Ipp32f")
+    {
+        randGauss_test<Ipp32f>();
+    }
+
+    SECTION("Ipp64f")
+    {
+        randGauss_test<Ipp64f>();
     }
 }
