dea13b67bb746cd95ca33d5d6378c878e7cc8de4
unknown <icyveins@hotmail.com>
2023-08-23 18:27:55 +0800

    edit dft_example to print output

--- examples/dft_example.cpp
+++ examples/dft_example.cpp
@@ -6,15 +6,23 @@ cl /EHsc /O2 dft_example.cpp ippcore.lib ipps.lib /I"../include/"
 
 int main()
 {
-    // make the DFT object
-    ippe::DFTCToC<Ipp32fc> dft(100);
-
     // some data
-    ippe::vector<Ipp32fc> in(100);
-    ippe::vector<Ipp32fc> out(100);
+    ippe::vector<Ipp32fc> in(10);
+    for (int i = 0; i < in.size(); i++)
+    {
+        in[i].re = i;
+        in[i].im = i;
+    }
+    ippe::vector<Ipp32fc> out(in.size(), {0.0f, 0.0f});
+
+    // make the DFT object
+    ippe::DFTCToC<Ipp32fc> dft(out.size());
 
     // execute it
     dft.fwd(in.data(), out.data());
+
+    for (int i = 0; i < out.size(); i++)
+        printf("%f %f\n", out[i].re, out[i].im);
 
     return 0;
 }
