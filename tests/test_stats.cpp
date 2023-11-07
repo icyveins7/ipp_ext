5bf8663cf136076404e4b4595fb62bcd87221d5b
seomac <icyveins@hotmail.com>
2023-11-07 12:55:57 +0800

    complete tests for DotProd, passing
    need to include tests for Sfs flavours

--- tests/test_stats.cpp
+++ tests/test_stats.cpp
@@ -273,3 +273,143 @@ void test_MinIndx()
     REQUIRE(std::abs(min - checkMin) < 1e-10);
     REQUIRE(idx == checkIdx);
 }
+
+TEST_CASE("ippe stats MinIndx", "[stats], [MinIndx]")
+{
+    SECTION("Ipp16s"){
+        test_MinIndx<Ipp16s>();
+    }
+    SECTION("Ipp32s"){
+        test_MinIndx<Ipp32s>();
+    }
+    SECTION("Ipp32f"){
+        test_MinIndx<Ipp32f>();
+    }
+    SECTION("Ipp64f"){
+        test_MinIndx<Ipp64f>();
+    }
+}
+
+// =====================================================================================================================
+template <typename T, typename U, typename V>
+void test_DotProd_realreal_real()
+{
+    ippe::vector<T> x(10);
+    ippe::vector<U> y(10);
+    V z;
+
+    // Make some values
+    for (int i = 0; i < 10; i++)
+    {
+        x[i] = (T)(i+10);
+        y[i] = (U)(i+1);
+    }
+
+    ippe::stats::DotProd(x.data(), y.data(), x.size(), &z);
+
+    V checkZ = 0;
+    for (int i = 0; i < 10; i++){
+        checkZ += x[i] * y[i];
+    }
+    REQUIRE(std::abs(z - checkZ) < 1e-7);
+}
+
+template <typename T, typename U, typename V>
+void test_DotProd_realcomplex_complex()
+{
+    ippe::vector<T> x(10);
+    ippe::vector<U> y(10);
+    V z;
+
+    // Make some values
+    for (int i = 0; i < 10; i++)
+    {
+        x[i] = (T)(i+10);
+        y[i].re = (i+1);
+        y[i].im = (i+2);
+    }
+
+    ippe::stats::DotProd(x.data(), y.data(), x.size(), &z);
+
+    V checkZ = {0, 0};
+    for (int i = 0; i < 10; i++){
+        checkZ.re += x[i] * y[i].re;
+        checkZ.im += x[i] * y[i].im;
+    }
+    REQUIRE(std::abs(z.re - checkZ.re) < 1e-7);
+    REQUIRE(std::abs(z.im - checkZ.im) < 1e-7);
+}
+
+template <typename T, typename U, typename V>
+void test_DotProd_complexcomplex_complex()
+{
+    ippe::vector<T> x(10);
+    ippe::vector<U> y(10);
+    V z;
+
+    // Make some values
+    for (int i = 0; i < 10; i++)
+    {
+        x[i].re = (i+1);
+        x[i].im = (i+2);
+        y[i].re = (i+3);
+        y[i].im = (i+4);
+    }
+
+    ippe::stats::DotProd(x.data(), y.data(), x.size(), &z);
+
+    V checkZ = {0, 0};
+    for (int i = 0; i < 10; i++){
+        checkZ.re += x[i].re * y[i].re - x[i].im * y[i].im;
+        checkZ.im += x[i].re * y[i].im + x[i].im * y[i].re;
+    }
+    REQUIRE(std::abs(z.re - checkZ.re) < 1e-7);
+    REQUIRE(std::abs(z.im - checkZ.im) < 1e-7);
+}
+
+TEST_CASE("ippe stats DotProd", "[stats], [DotProd]")
+{
+    SECTION("Ipp32f, Ipp32f -> Ipp32f"){
+        test_DotProd_realreal_real<Ipp32f, Ipp32f, Ipp32f>();
+    }
+    SECTION("Ipp32fc, Ipp32fc -> Ipp32fc"){
+        test_DotProd_complexcomplex_complex<Ipp32fc, Ipp32fc, Ipp32fc>();
+    }
+    SECTION("Ipp32f, Ipp32fc -> Ipp32fc"){
+        test_DotProd_realcomplex_complex<Ipp32f, Ipp32fc, Ipp32fc>();
+    }
+
+    SECTION("Ipp32f, Ipp32f -> Ipp64f"){
+        test_DotProd_realreal_real<Ipp32f, Ipp32f, Ipp64f>();
+    }
+    SECTION("Ipp32fc, Ipp32fc -> Ipp64fc"){
+        test_DotProd_complexcomplex_complex<Ipp32fc, Ipp32fc, Ipp64fc>();
+    }
+    SECTION("Ipp32f, Ipp32fc -> Ipp64fc"){
+        test_DotProd_realcomplex_complex<Ipp32f, Ipp32fc, Ipp64fc>();
+    }
+
+    SECTION("Ipp64f, Ipp64f -> Ipp64f"){
+        test_DotProd_realreal_real<Ipp64f, Ipp64f, Ipp64f>();
+    }
+    SECTION("Ipp64fc, Ipp64fc -> Ipp64fc"){
+        test_DotProd_complexcomplex_complex<Ipp64fc, Ipp64fc, Ipp64fc>();
+    }
+    SECTION("Ipp64f, Ipp64fc -> Ipp64fc"){
+        test_DotProd_realcomplex_complex<Ipp64f, Ipp64fc, Ipp64fc>();
+    }
+
+    SECTION("Ipp16s, Ipp16s -> Ipp64s"){
+        test_DotProd_realreal_real<Ipp16s, Ipp16s, Ipp64s>();
+    }
+    SECTION("Ipp16sc, Ipp16sc -> Ipp64sc"){
+        test_DotProd_complexcomplex_complex<Ipp16sc, Ipp16sc, Ipp64sc>();
+    }
+    SECTION("Ipp16s, Ipp16sc -> Ipp64sc"){
+        test_DotProd_realcomplex_complex<Ipp16s, Ipp16sc, Ipp64sc>();
+    }
+
+    SECTION("Ipp16s, Ipp16s -> Ipp32f"){
+        test_DotProd_realreal_real<Ipp16s, Ipp16s, Ipp32f>();
+    }
+}
