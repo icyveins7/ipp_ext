0e8c5fd9071b7bc3625d8181f07c5498a01c9809
seomac <icyveins@hotmail.com>
2023-12-16 22:19:00 +0800

    add complex tone tests (passing)
    TODO: add real tone tests, but probably not very important
    TODO: handle integer 16sc tests, also probably not important

--- tests/test_generators.cpp
+++ tests/test_generators.cpp
@@ -71,3 +71,54 @@ TEST_CASE("ippe Slope sample generator", "[generator],[slope]")
         test_slope_sample_generator<Ipp64f, Ipp64f>();
     }
 }
+
+
+/*
+Template test case for Tone sample generators
+*/
+template <typename T, typename U, typename V>
+void test_tone_sample_generator_complex()
+{
+    // Make output vector
+    ippe::vector<T> out(10);
+
+    // Define magn and freq
+    U magn = 1;
+    V freq = 0.1;
+
+    // Define phase
+    V phase = 0;
+
+    // Generate tone samples into it
+    ippe::generator::Tone(
+        out.data(), out.size(),
+        magn, freq, &phase
+    );
+
+    // Check if the samples are correct
+    for (int i = 0; i < out.size(); ++i)
+    {
+        double real = magn * std::cos(2*M_PI*freq*i + phase);
+        double imag = magn * std::sin(2*M_PI*freq*i + phase);
+        REQUIRE(out[i].re - real < 1e-5);
+        REQUIRE(out[i].im - imag < 1e-5); // arbitrary threshold
+    }
+}
+
+TEST_CASE("ippe Tone sample generator", "[generator],[tone]")
+{
+    // Comparing integer output is a bit weird and i'm lazy to write special test for this
+    // SECTION("Ipp16sc output"){
+    //     test_tone_sample_generator_complex<Ipp16sc, Ipp16s, Ipp32f>();
+    // }
+
+    SECTION("Ipp32fc output"){
+        test_tone_sample_generator_complex<Ipp32fc, Ipp32f, Ipp32f>();
+    }
+    
+    SECTION("Ipp64fc output"){
+        test_tone_sample_generator_complex<Ipp64fc, Ipp64f, Ipp64f>();
+    }
+
+    // TODO: test real tone output
+}
