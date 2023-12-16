eac53e2672c51c161946fbdae0e05699b1fb4ec0
seomac <icyveins@hotmail.com>
2023-12-16 22:01:31 +0800

    add benchmark operations for complex and real/complex conversions

--- benchmarks/benchmark_ops.cpp
+++ benchmarks/benchmark_ops.cpp
@@ -9,7 +9,7 @@
 // Also include benchmarking headers, i don't really know which one is necessary
 #include <catch2/benchmark/catch_benchmark.hpp>
 
-TEST_CASE("Benchmark (real) array, array to (real) array operations, length 100000")
+TEST_CASE("Benchmark (real) array, array to (real) array operations, length 100000", "[real],[basic]")
 {
     // Setup
     const int length = 100000;
@@ -36,4 +36,54 @@ TEST_CASE("Benchmark (real) array, array to (real) array operations, length 1000
     };
 
 
+}
+
+TEST_CASE("Benchmark (complex) array, array to (complex) array operations, length 100000", "[complex],[basic]")
+{
+    // Setup
+    const int length = 100000;
+    ippe::vector<Ipp32fc> in1(length);
+    ippe::vector<Ipp32fc> in2(length);
+    ippe::vector<Ipp32fc> out(length);
+
+    BENCHMARK("ippsAdd_32fc")
+    {
+        return ippe::math::Add<Ipp32fc>(
+            in1.data(), in2.data(), out.data(), length);
+    };
+
+    BENCHMARK("ippsSub_32fc")
+    {
+        return ippe::math::Sub<Ipp32fc>(
+            in1.data(), in2.data(), out.data(), length);
+    };
+
+    BENCHMARK("ippsMul_32fc")
+    {
+        return ippe::math::Mul<Ipp32fc>(
+            in1.data(), in2.data(), out.data(), length);
+    };
+}
+
+TEST_CASE("Benchmark conversions between complex and real arrays, length 100000", "[convert]")
+{
+    // Setup
+    const int length = 100000;
+    ippe::vector<Ipp32f> real1(length);
+    ippe::vector<Ipp32f> real2(length);
+    ippe::vector<Ipp32fc> complex1(length);
+
+    BENCHMARK("ippsPolarToCart_32fc")
+    {
+        return ippe::convert::PolarToCart(
+            real1.data(), real2.data(), complex1.data(), length
+        );
+    };
+
+    BENCHMARK("ippsCartToPolar_32fc")
+    {
+        return ippe::convert::CartToPolar(
+            complex1.data(), real1.data(), real2.data(), length
+        );
+    };
 }
