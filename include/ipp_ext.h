ae8cdfda9cf85305c4c8a18a10e1a96ba650fdfd
seo <icyveins@hotmail.com>
2024-07-05 13:24:18 +0800

    modify folder structure in preparation for image processing

--- include/ipp_ext.h
+++ include/ipp_ext.h
@@ -1,15 +1,19 @@
 #pragma once
 
-#include "ipp_ext_vec.h"
-#include "ipp_ext_dft.h"
-#include "ipp_ext_filter.h"
-#include "ipp_ext_random.h"
-#include "ipp_ext_matrix.h"
 #include "ipp_ext_errors.h"
-#include "ipp_ext_math.h"
-#include "ipp_ext_generator.h"
-#include "ipp_ext_convert.h"
-#include "ipp_ext_transform.h"
-#include "ipp_ext_stats.h"
-#include "ipp_ext_logical.h"
-#include "ipp_ext_sampling.h"
+
+// Signal
+#include "signal/ipp_ext_vec.h"
+#include "signal/ipp_ext_dft.h"
+#include "signal/ipp_ext_filter.h"
+#include "signal/ipp_ext_random.h"
+#include "signal/ipp_ext_matrix.h"
+#include "signal/ipp_ext_math.h"
+#include "signal/ipp_ext_generator.h"
+#include "signal/ipp_ext_convert.h"
+#include "signal/ipp_ext_transform.h"
+#include "signal/ipp_ext_stats.h"
+#include "signal/ipp_ext_logical.h"
+#include "signal/ipp_ext_sampling.h"
+
+// Image
